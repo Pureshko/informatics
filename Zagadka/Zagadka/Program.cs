cb522000c5bf278aae4384e28aa56ea0aefc7302
Pureshko <kotleta552@gmail.com>
2020-10-02 01:21:18 +0600

    first commit

---
+++ Zagadka/Zagadka/Program.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Zagadka
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string ru = "йцукенгшщзхъфывапролджэ€чсмитьбю.,10 ";
+            string en = "qwertyuiop[]asdfghjkl;'zxcvbnm,.?/10_";
+            string cipher = "cgfcb,j_pf_exfcnbt_d_lfyyjv_vbyb_rdtcn_fdnjh_bltb_b_ghjuhfvv_fqn,ftd_lfektn_bp_10l?dctv_byajhvfnbrfv_cgfcb,j_pf_nj/_xnj_lftnt_pyfybt_j_rjvgm.ntht_b_yfdsrb_ghjuhfvvbhjdfybt/_tckb_,s_z_yt_pyfk_rfr_ghjuhfvvbhjdfnm?_nj_yt yfgbcfk_,s_dtcm_'njn_rjyrehc/_tot_ghjie_gjctnbnm _cfqn_c_hfyljvysvb_gjplhfdktybzvb_exbntktq/";
+            string site = "http://a0474181.xsph.ru/s";
+            string s = "";
+            
+            Translater();
+            void Translater()
+            {
+                //напишите код который помен€ет раскладку с руского на английский()
+                
+                Console.WriteLine(s + site);
+            }
+        }
+
+        
+    }
+}
